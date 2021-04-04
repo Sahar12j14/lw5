84f3ad6fd1a54919230e25f8ceeda7d80ce61bba
Sahar12j14 <71217328+Sahar12j14@users.noreply.github.com>
2021-04-04 09:58:18 +0300

    update

--- src/test/observable-structures.test.js
+++ src/test/observable-structures.test.js
@@ -3,65 +3,24 @@ const productMethods = require('../utils/observable-structures');
 
 const mock = jest.fn();
 test('checking the created observable object', () => {
-  const productElements = [
+  const testObject = 
     {
-      id: 1,
-      name: 'Молоко',
-      count: 40,
-      priceForOne: 50,
-      priceTotal: 2000,
-    },
-    {
-      id: 2,
-      name: 'Хлеб',
-      count: 100,
-      priceForOne: 20,
-      priceTotal: 2000,
-    },
-    {
-      id: 3,
-      name: 'Лук',
-      count: 200,
-      priceForOne: 5,
-      priceTotal: 1000,
-    },
-  ];
-  let resultList = [];
-  resultList[0] = productMethods.createObservableObject(productElements[0], mock);
-  resultList[0].count = 1;
-  expect(resultList[0].count).toEqual(1);
-  expect(productMethods.createObservableObject(productElements[0], 1)).toEqual(false);
+        test: 'test'
+    }
+  ;
+  const resultList = productMethods.createObservableObject(testObject, mock);
+  resultList.test = 'test2';
+  expect(resultList.test).toEqual('test2');
+  expect(productMethods.createObservableObject(testObject, 1)).toEqual(false);
   expect(productMethods.createObservableObject(1, mock)).toEqual(false);
   expect(productMethods.createObservableObject(false, mock)).toEqual(false);
   expect(productMethods.createObservableObject(null, mock)).toEqual(false);
   expect(productMethods.createObservableObject(null, 1)).toEqual(false);
-  expect(productMethods.createObservableObject(productElements[0], '')).toEqual(false);
+  expect(productMethods.createObservableObject(testObject, '')).toEqual(false);
   expect(productMethods.createObservableObject('', mock)).toEqual(false);
 });
 test('checking the created observable array', () => {
-  const productElements = [
-    {
-      id: 1,
-      name: 'Молоко',
-      count: 40,
-      priceForOne: 50,
-      priceTotal: 2000,
-    },
-    {
-      id: 2,
-      name: 'Хлеб',
-      count: 100,
-      priceForOne: 20,
-      priceTotal: 2000,
-    },
-    {
-      id: 3,
-      name: 'Лук',
-      count: 200,
-      priceForOne: 5,
-      priceTotal: 1000,
-    },
-  ];
+  const productElements = [1,2,3];
   let resultList = [];
   resultList[0] = productMethods.createObservableObject(productElements[0], mock);
   resultList = productMethods.createObservableArray(resultList, mock);
@@ -72,12 +31,4 @@ test('checking the created observable array', () => {
   expect(productMethods.createObservableArray(null, 1)).toEqual(false);
   expect(productMethods.createObservableArray(resultList, '')).toEqual(false);
   expect(productMethods.createObservableArray('', mock)).toEqual(false);
-  resultList = 1;
-  expect(resultList).toEqual(1);
-  expect(resultList).not.toEqual(2);
-  expect(resultList).not.toEqual(NaN);
-  expect(resultList).not.toEqual(true);
-  expect(resultList).not.toEqual('');
-  expect((resultList = '')).toEqual('');
-  expect((resultList = NaN)).toEqual(NaN);
 });
