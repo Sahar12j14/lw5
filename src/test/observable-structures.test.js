d233380f6cde9ee5d33a64f8bf9a930cc14e75e4
Sahar12j14 <71217328+Sahar12j14@users.noreply.github.com>
2021-04-08 01:49:49 +0300

    update

--- src/test/observable-structures.test.js
+++ src/test/observable-structures.test.js
@@ -1,34 +1,21 @@
 /* eslint-disable no-undef */
 const productMethods = require('../utils/observable-structures');
-
-const mock = jest.fn();
+const mockedCallback = jest.fn();
 test('checking the created observable object', () => {
-  const testObject = 
-    {
-        test: 'test'
-    }
-  ;
-  const resultList = productMethods.createObservableObject(testObject, mock);
+  const testObject = {
+    test: 'test',
+  };
+  const resultList = productMethods.createObservableObject(testObject, mockedCallback);
   resultList.test = 'test2';
   expect(resultList.test).toEqual('test2');
   expect(productMethods.createObservableObject(testObject, 1)).toEqual(false);
-  expect(productMethods.createObservableObject(1, mock)).toEqual(false);
-  expect(productMethods.createObservableObject(false, mock)).toEqual(false);
-  expect(productMethods.createObservableObject(null, mock)).toEqual(false);
-  expect(productMethods.createObservableObject(null, 1)).toEqual(false);
-  expect(productMethods.createObservableObject(testObject, '')).toEqual(false);
-  expect(productMethods.createObservableObject('', mock)).toEqual(false);
+  expect(productMethods.createObservableObject(testObject, mockedCallback)).not.toEqual(false);
+  //expect(mockCallback.mock.calls.length).toBe(1);
 });
 test('checking the created observable array', () => {
-  const productElements = [1,2,3];
-  let resultList = [];
-  resultList[0] = productMethods.createObservableObject(productElements[0], mock);
-  resultList = productMethods.createObservableArray(resultList, mock);
-  expect(productMethods.createObservableArray(resultList, 1)).toEqual(false);
-  expect(productMethods.createObservableArray(1, mock)).toEqual(false);
-  expect(productMethods.createObservableArray(false, mock)).toEqual(false);
-  expect(productMethods.createObservableArray(null, mock)).toEqual(false);
-  expect(productMethods.createObservableArray(null, 1)).toEqual(false);
-  expect(productMethods.createObservableArray(resultList, '')).toEqual(false);
-  expect(productMethods.createObservableArray('', mock)).toEqual(false);
+  const resultList = [1, 2];
+  let testList = productMethods.createObservableArray(resultList, mockedCallback);
+  expect(productMethods.createObservableArray(testList, 1)).toEqual(false);
+  expect(productMethods.createObservableArray(testList, mockedCallback)).not.toEqual(false);
+  //expect(mockCallback.mock.calls.length).toBe(3);
 });
